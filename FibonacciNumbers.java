0e52cde4c79848180d0353c6b2a930c30ec2bf70
VANIKUMARIGULLA <154726581+VANIKUMARIGULLA@users.noreply.github.com>
2024-12-02 22:33:54 +0530

    Update FibonacciNumbers.java

--- FibonacciNumbers.java
+++ FibonacciNumbers.java
@@ -1,5 +1,10 @@
 public class FibonacciNumbers {
     public static void main(String[] args){
+        /*
+        0+1 = 1,
+        1+1 = 2,
+    
+        */
         int a = 0, b = 1, c, count = 10;
         System.out.println(a);
         System.out.println(b);
